8ad81ff56400902b185dead622545907bce0b515
Agustin Parmisano <agustinparmisano@gmail.com>
2017-03-26 21:15:07 -0300

    First Commit, testing mqtt python with esp8266 & acs712

--- mqtt_python_server.py
+++ mqtt_python_server.py
@@ -3,12 +3,15 @@ import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
 import time
 from flask import Flask
+from flask.ext.pymongo import PyMongo
 import json
 
 app = Flask(__name__)
 
-list = []
-auxmsg =  ""
+app.config['MONGO_DBNAME'] = 'restdb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+
+mongo = PyMongo(app)
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -20,13 +23,13 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
-    auxmsg = msg
-
-    list = json.loads(auxmsg.payload)
+    list = json.loads(msg.payload)
 
     for key,value in list.iteritems():
         print ("")
         print key, value
+
+    add_measure(list);
 
 def on_subscribe(client, userdata,mid, granted_qos):
     print "userdata : " +str(userdata)
@@ -41,16 +44,31 @@ client.on_message = on_message
 client.connect("localhost", 1883, 60)
 client.loop_start()
 
+def add_measure(list):
+  measure = mongo.db.measures
+  ip = list['ip']
+  time = list['time']
+  name = list['name']
+  data = list['data']
+  measure_id = measure.insert({'ip': ip, 'time': time, 'name': name, 'data': data})
+  new_measure = measure.find_one({'_id': measure_id })
+  output = {'name' : new_measure['name'], 'data' : new_measure['data']}
+  return jsonify({'result' : output})
+
 @app.route("/")
 def hello():
     return "Hello World!"
 
 @app.route("/data")
-def getDataInSensor():
+def get_all_measures():
     print("Sending 2...")
     publish.single("ledStatus", "2", hostname="localhost")
 
-    return "Data"
+    measure = mongo.db.mueasures
+    output = []
+    for m in measure.find():
+        output.append({'name' : m['name'], 'data' : m['data']})
+    return jsonify({'result' : output})
 
 @app.route("/off")
 def relay_ff():
